5bdeaace70981014fb60e1533c2f610b51eeea35
Seoyeon Ko <55572222+seoyeonko@users.noreply.github.com>
2022-06-18 14:00:57 +0900

    3차 과제 시작전 백업

--- demo/src/main/java/com/example/demo/config/WebMvcConfig.java
+++ demo/src/main/java/com/example/demo/config/WebMvcConfig.java
@@ -10,7 +10,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-		  .allowedOrigins("http://localhost:3000")
+		  .allowedOrigins("http://localhost:3000", "http://prod-todo-frontend-20181023-sean.us-west-2.elasticbeanstalk.com")
 		  .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
 		  .allowedHeaders("*")
 		  .allowCredentials(true)
