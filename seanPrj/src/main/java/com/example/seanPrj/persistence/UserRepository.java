8d5bcb58d18f925407715a731968f945eede4566
Seoyeon Ko <55572222+seoyeonko@users.noreply.github.com>
2022-06-18 16:02:14 +0900

    [seanPrj] 4.2 회원가입, 로그인 구현

---
+++ seanPrj/src/main/java/com/example/seanPrj/persistence/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.seanPrj.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.seanPrj.model.UserEntity;
+
+public interface UserRepository extends JpaRepository<UserEntity, String> {
+	UserEntity findByEmail(String email);
+	Boolean existsByEmail(String email);
+	UserEntity findByEmailAndPassword(String email, String password);
+
+}
